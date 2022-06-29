37a6d2aa1296b57f823a9b4c3a7ddf98ae261218
Bhavya Verma <vbhavya269@gmail.com>
2022-06-29 14:36:16 +0530

    ✨ Added Mountain Array code

--- src/com/Array/BinarySearch/mountainArray.java
+++ src/com/Array/BinarySearch/mountainArray.java
@@ -1,20 +1,90 @@
 package com.Array.BinarySearch;
 
+// https://leetcode.com/problems/find-in-mountain-array/
+
 public class mountainArray {
     public static void main(String[] args) {
 
     }
     
-    public static int Mountain(int[] arr) {
+    int search(int[] arr, int target) {
+        int peak = peakInMountain(arr);
+        int firstTry = orderAgnosticBS(arr, target, 0, peak);
+
+        if (firstTry != -1) {
+            return firstTry;
+        } else {
+            // Try to search in second half of array
+            return orderAgnosticBS(arr, target, peak, arr.length - 1);
+        }
+    }
+    
+    static int orderAgnosticBS(int[] arr, int target, int start, int end) {
+
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+            // find the middle element
+            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds
+            // the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    public static int peakInMountain(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
 
         while (start < end) {
             int mid = start + (end - start) / 2;
-            
-            if (arr[mid] < arr[mid + 1]) {
-                
+
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
             }
         }
+
+        return start;
+    }
+    
+    static int BinarySearch(int arr[], int target, int start, int end) {
+
+        while (start <= end) {
+            // find the middle element
+            // int mid = (start + end) / 2; // might be possible (start +end) exceeds range
+            // of int
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return -1;
     }
 }
